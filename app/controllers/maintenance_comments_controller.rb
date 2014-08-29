003c705c73df13de73c2065d669bd8ad16c92e40
Amir Kanpurwala <akanpurwala@gmail.com>
2014-08-29 10:56:32 -0700

    Added AJAX for adding / deleing comments

--- app/controllers/maintenance_comments_controller.rb
+++ app/controllers/maintenance_comments_controller.rb
@@ -1,15 +1,33 @@
 class MaintenanceCommentsController < ApplicationController
+  respond_to :html, :js
   def create
     @mr = MaintenanceRequest.find(params[:maintenance_request_id])
     @comments = @mr.maintenance_comments
     @comment = current_user.maintenance_comments.build(params[:maintenance_comment])
     @comment.maintenance_request = @mr
+    @new_comment = MaintenanceComment.new
     if @comment.save
       flash[:notice] = "Your comment was created."
-      redirect_to @mr
     else
       flash[:error] = "There was an error creating your comment. Please try again."
-      render 'maintenance_requests/show'
+    end
+
+    respond_with(@comment) do |f|
+      f.html { redirect_to @mr }
     end
   end
+
+  def destroy
+    @mr = MaintenanceRequest.find(params[:maintenance_request_id])
+    @comment = @mr.maintenance_comments.find(params[:id])
+    if @comment.destroy
+      flash[:notice] = "You have successfully deleted your comment."
+    else
+      flash[:error] = "There was an error deleting your comment. Please try again."
+    end
+    respond_with(@comment) do |f|
+      f.html { redirect_to @mr }
+    end
+  end
+
 end
